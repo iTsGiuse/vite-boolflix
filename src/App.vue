a51296583a4c573744650655fe7dc8cbd4fe1657
iTsGiuse <giuse.amato@icloud.com>
2024-04-17 16:00:33 +0200

    new exercise

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+  import { store } from './store.js';
+  import AppHeader from './components/Header/AppHeader.vue';
+
+  export default {
+    name: AppHeader,
+  }
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
